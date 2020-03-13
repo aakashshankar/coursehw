682ab1977ab685598e7a893aee1270722ed55f52
aakash <aakashshankar217@gmail.com>
2020-03-13 17:56:44 +0530

    Adding CSV file and fixing encoding

--- pandas_url_test.py
+++ pandas_url_test.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from pandas import DataFrame
-names=['user_id','age','sex','occupation','zip code']
-df=pd.read_csv('http://files.grouplens.org/datasets/movielens/ml-100k/u.user',sep='|',names=names)
-print(df)
-print(df.head())
+movie_cols=['movie_id', 'title', 'release_date','video_release_date', 'imdb_url']
+movies=pd.read_csv('ratings_cleaned.csv',sep='|',names=movie_cols,encoding='utf-8')
+print(movies)
+rating_cols=['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings=pd.read_csv('http://files.grouplens.org/datasets/movielens/ml-100k/u.data',sep='\t',names=rating_cols)
+print(ratings)
+ratings.merge(movies, on='movie_id')
+avg_rating_per_movie=(ratings['rating'].groupby(ratings['movie_id'])).mean()
+print(avg_rating_per_movie.max())
+print(ratings)
